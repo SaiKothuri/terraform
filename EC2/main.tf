635c16ade1d158424c1a097b5276066e5476f0b5
SaiKothuri <159695557+SaiKothuri@users.noreply.github.com>
2024-06-10 20:52:57 +0530

    Update main.tf
    Updated AMI iD

--- EC2/main.tf
+++ EC2/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "ec2" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = "ami-0c55b159cbfafe1f0" # us-west-2
   instance_type = "t2.micro"
   }
