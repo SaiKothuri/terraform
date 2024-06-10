1fc545042c89123c43691a70357fb4bcecba6131
SaiKothuri <159695557+SaiKothuri@users.noreply.github.com>
2024-06-10 21:26:11 +0530

    Update main.tf
    Creating VPC with Varaible.tf

--- EC2 Instance/main.tf
+++ EC2 Instance/main.tf
@@ -1,1 +1,9 @@
+# 
+# VPC Creation
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.cidr_block                #"172.16.0.0/16"
 
+  tags = {
+    name =  var.name                         #"vpc"
+  }
+}
