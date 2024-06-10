e3f7dda68e879abb29e1e50c798e75efc7272ebf
SaiKothuri <159695557+SaiKothuri@users.noreply.github.com>
2024-06-10 20:37:19 +0530

    Update main.tf
    Creating sample EC2 instance

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,1 +1,10 @@
-
+#Creating EC2
+#Using Resource Block
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+resource "aws_instance" "ec2" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+  }
