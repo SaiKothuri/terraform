8812a1b2eecad02d3c2d5315597b2beabc482d90
SaiKothuri <159695557+SaiKothuri@users.noreply.github.com>
2024-06-10 21:11:46 +0530

    Update main.tf
    Adding VPC, Subnet, Network Interface

--- EC2/main.tf
+++ EC2/main.tf
@@ -7,4 +7,37 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-0c55b159cbfafe1f0" # us-west-2
   instance_type = "t2.micro"
+network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
   }
+  }
+# VPC Creation
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+# Subnet Creation
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "subnet"
+  }
+}
+
+# Network Interface
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
