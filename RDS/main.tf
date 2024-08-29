945dd287e7c64dd6e7c6c11509c631e501817aac
SaiKothuri <159695557+SaiKothuri@users.noreply.github.com>
2024-08-29 09:53:28 +0530

    Update main.tf
    RDS main

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,1 +1,12 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    =  var.allocated_storage                        #10
+  db_name              =  var.db_name                                  #"mydb"
+  engine               =  var.engine                                   #"mysql"
+  engine_version       =  var.engine_version                           #"8.0"
+  instance_class       =  var.instance_class                           #"db.t3.micro"
+  username             =  var.username                                 #"foo"
+  password             =  var.password                                 #"foobarbaz"
+  parameter_group_name =  var.parameter_group_name                     #"default.mysql8.0"
+  skip_final_snapshot  =  var.skip_final_snapshot                      #true
+}
 
