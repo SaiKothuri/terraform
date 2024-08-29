295e17f077ca8c11e9ab26ca30c271577581c26a
SaiKothuri <159695557+SaiKothuri@users.noreply.github.com>
2024-08-29 09:52:51 +0530

    Update variable.tf
    RDS var.tf

--- RDS/variable.tf
+++ RDS/variable.tf
@@ -1,1 +1,38 @@
+variable "allocated_storage"{
+     type = number
+     description = ""
+}
+variable "db_name"{
+     type = string
+     description = ""
+}
+variable "engine"{
+     type = string
+     description = ""
+}
+variable "engine_version"{
+     type = number
+     description = ""
+}
+variable "instance_class"{
+     type = string
+     description = ""
+}
+variable "username"{
+     type = string
+     description = ""
+}
+variable "password"{
+     type = string
+     description = ""
+}
+variable "parameter_group_name"{
+     type = string
+     description = ""
+}
+variable "skip_final_snapshot"{
+     type = bool
+     description = ""
+}
+
 
